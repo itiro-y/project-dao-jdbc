ced55e3d8852c8abfb04a16aa9cb2d89bc29dd4b
Ayrton Itiro Kobo Yoshii <itiro_yoshii@hotmail.com>
2024-11-22 01:33:43 -0300

    Add Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,14 +3,18 @@ package application;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Date;
 
 import db.DB;
 import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Department department = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, department);
+		System.out.println(seller);
 	}
 }
